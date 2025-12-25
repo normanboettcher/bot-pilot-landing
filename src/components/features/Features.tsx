0afaefd9b4e0d57d5f8abeacea09890963f10829
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-12-25 12:16:01 +0100

    [normanboettcher#20] Added - Features component v1

---
+++ src/components/features/Features.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import type { Feature } from './domain/Feature.ts';
+import TextDecoration from '../decoration/TextDecoration.tsx';
+import { Stack } from '@mui/material';
+import useResponsive from '../../core/useResponsive.ts';
+import FeaturesCard from './FeaturesCard.tsx';
+import featureImage from '../../assets/images/feature1.png';
+
+const features: Feature[] = [
+  {
+    title: 'Gestalten Sie die Sprache Ihres Bots individuell nach Ihren Wünschen',
+    imageUrl: featureImage,
+    content: (
+      <span>
+        Was für ein <TextDecoration>tolles Feature</TextDecoration>
+      </span>
+    ),
+  },
+  {
+    title: 'Feature2',
+    imageUrl: featureImage,
+    content: (
+      <span>
+        Was für ein <TextDecoration>tolles Feature</TextDecoration>
+      </span>
+    ),
+  },
+  {
+    title: 'Feature3',
+    imageUrl: featureImage,
+    content: (
+      <span>
+        Was für ein <TextDecoration>tolles Feature</TextDecoration>
+      </span>
+    ),
+  },
+];
+
+const Features = () => {
+  const { isMobile } = useResponsive();
+  return (
+    <Stack direction={'column'} p={1} spacing={1}>
+      {features.map((feature, index) => (
+        <FeaturesCard
+          key={index}
+          direction={isMobile ? 'column' : index % 2 === 0 ? 'row' : 'row-reverse'}
+          feature={feature}
+        />
+      ))}
+    </Stack>
+  );
+};
+
+export default Features;
