fd33ebbda57dd2880bee38af522b55c609ae2564
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-12-28 16:31:49 +0100

    [normanboettcher/bot-pilot-landing#31] Added - ContactForm v1

--- src/components/buttons/ContactUs.tsx
+++ src/components/buttons/ContactUs.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Box, Button } from '@mui/material';
+import ContactFormDialog from '../contact/ContactFormDialog.tsx';
+import { useContactDialog } from '../../context/DialogContext.tsx';
 
 const ContactUsButton = () => {
+  const { setOpen, isOpen, onClose, onSubmit } = useContactDialog();
+  const onClick = () => {
+    setOpen(true);
+  };
   return (
-    <Box display={'flex'} justifyContent={'center'} p={1} pt={2}>
-      <Button variant={'contained'} color={'secondary'}>
-        JETZT KONTAKT AUFNEHMEN!
-      </Button>
+    <Box>
+      <Box display={'flex'} justifyContent={'center'} p={1} pt={2}>
+        <Button variant={'contained'} color={'secondary'} onClick={onClick}>
+          JETZT KONTAKT AUFNEHMEN!
+        </Button>
+      </Box>
+      <ContactFormDialog open={isOpen} onClose={onClose} onSubmit={onSubmit} />
     </Box>
   );
 };
---
+++ src/context/DialogContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useState } from 'react';
+import React from 'react';
+export const DialogContext = createContext<{
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  setOpen: (open: boolean) => void;
+}>({
+  isOpen: false,
+  onClose: () => {},
+  onSubmit: () => {},
+  setOpen: () => {},
+});
+
+export const DialogContextProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <DialogContext.Provider
+      value={{
+        isOpen: open,
+        onClose: onClose,
+        onSubmit: () => {},
+        setOpen: setOpen,
+      }}
+    >
+      {children}
+    </DialogContext.Provider>
+  );
+};
+export const useContactDialog = () => {
+  const context = useContext(DialogContext);
+  if (!context) {
+    throw new Error('useDialogContent() must be used within a DialogContextProvider');
+  }
+  return context;
+};
