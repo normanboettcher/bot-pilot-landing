d7bda97ad3646514fc71e82797c7309e8cb023e9
Norman-Ian Böttcher <norman.boettcher@web.de>
2025-12-28 08:30:54 +0100

    [normanboettcher/bot-pilot-landing#27] Added - highlightsV2

---
+++ src/components/advantages/v2/HighlightsV2.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Box, Grid } from '@mui/material';
+import { getProductAdvantages } from '../ProductAdvantages.tsx';
+import HighlightsCard from './HighlightsCard.tsx';
+import ParagraphContainer from '../../containers/ParagraphContainer.tsx';
+import AdvantagesHeading from '../AdvantagesHeading.tsx';
+
+const HighlightsV2 = () => {
+  const features = getProductAdvantages();
+  function highlightV2() {
+    return (
+      <Box>
+        <Grid container spacing={2}>
+          {features.map((advantage, index) => (
+            <Grid size={{ xs: 12, sm: 8, md: 4 }}>
+              <HighlightsCard key={index} highlight={advantage} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    );
+  }
+  return (
+    <Box sx={{ py: { xs: 8, sm: 12 } }}>
+      <ParagraphContainer heading={<AdvantagesHeading />} content={highlightV2()} />
+    </Box>
+  );
+};
+
+export default HighlightsV2;
