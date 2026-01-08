5806cbcda1a9429ac5fa0a2f16b7f239b06d2005
Norman-Ian Böttcher <norman.boettcher@web.de>
2026-01-08 20:33:33 +0100

    [normanboettcher/bot-pilot-landing#37] Added - ContactFormMailServiceImpl content

--- contact-form-mail/src/main/java/de/bot/pilot/mail/contact/service/ContactFormMailSenderService.java
+++ contact-form-mail/src/main/java/de/bot/pilot/mail/contact/service/ContactFormMailSenderService.java
@@ -1,10 +1,33 @@
 package de.bot.pilot.mail.contact.service;
 
 import de.bot.pilot.mail.contact.dto.ContactRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ContactFormMailSenderService implements ContactFormMailSender {
+    @Value("${spring.mail.username}")
+    private String username;
+
+    private final JavaMailSender mailSender;
+
+    public ContactFormMailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     @Override
     public void sendContactFormMail(ContactRequest contactRequest) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(username);
+        simpleMailMessage.setFrom(username);
 
+        String subjectTemplate = "Kontaktanfrage: %s";
+        var subject = subjectTemplate.formatted(contactRequest.company());
+
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(contactRequest.message());
+        mailSender.send(simpleMailMessage);
     }
 }
