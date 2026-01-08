00a14db4e0177f021a3209bcaf87a5598eff8237
Norman-Ian Böttcher <norman.boettcher@web.de>
2026-01-08 20:26:55 +0100

    [normanboettcher/bot-pilot-landing#37] Added - MailConfig Bean

--- contact-form-mail/src/main/java/de/bot/pilot/mail/contact/config/MailConfig.java
+++ contact-form-mail/src/main/java/de/bot/pilot/mail/contact/config/MailConfig.java
@@ -1,4 +1,31 @@
 package de.bot.pilot.mail.contact.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Configuration class for setting up the mail sender.
+ * This class provides the configuration for JavaMailSender.
+ */
+@Configuration
 public class MailConfig {
+
+    @Bean
+    @Primary
+    public JavaMailSender getJavaMailSender(JavaMailSenderImpl sender) throws IOException {
+        String pwFilePath = System.getenv("SMTP_PASSWORD_FILE");
+        if (pwFilePath != null) {
+            sender.setPassword(
+                    Files.readString(Path.of(pwFilePath)).trim()
+            );
+        }
+        return sender;
+    }
 }
