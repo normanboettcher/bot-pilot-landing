40552a084e458a381cdb9029f0f714aca8b41029
Norman-Ian Böttcher <norman.boettcher@web.de>
2026-01-11 10:41:55 +0100

    [normanboettcher/bot-pilot-landing#37] Added - prod Containerfile for contact-form-mail

--- contact-form-mail/deployment/prod/HealthCheck.java
+++ contact-form-mail/deployment/prod/HealthCheck.java
@@ -2,14 +2,33 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
 
-void main() throws IOException, InterruptedException {
-    var client = HttpClient.newHttpClient();
-    var request = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8080/actuator/health"))
-            .build();
-    var response = client.send(request, HttpResponse.BodyHandlers.discarding());
-    if (response.statusCode() != 200) {
-        System.exit(1);
+public class HealthCheck {
+    private static final Logger logger = Logger.getLogger(HealthCheck.class.getName());
+
+    /**
+     * Main method that checks the health of a service by sending an HTTP GET request
+     * to a predefined health check endpoint. If the status code of the response is not 200,
+     * it terminates the program with a non-zero exit status.
+     *
+     * @throws IOException          if an I/O error occurs when sending or receiving the HTTP request
+     * @throws InterruptedException if the operation is interrupted while waiting for the response
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        try (var client = HttpClient.newHttpClient()) {
+            logger.info("Sending health check request to localhost:8080/actuator/health");
+            var request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/actuator/health"))
+                    .build();
+            var response = client.send(request, HttpResponse.BodyHandlers.discarding());
+            if (response.statusCode() != 200) {
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            logger.severe("Health check failed: [" + e.getMessage() + "]");
+            System.exit(1);
+        }
     }
 }
