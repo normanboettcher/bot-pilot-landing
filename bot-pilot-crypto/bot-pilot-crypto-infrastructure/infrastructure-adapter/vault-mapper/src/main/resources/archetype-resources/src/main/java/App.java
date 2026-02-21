f3cb4d6218e32400a717e338d8e96ac6ed17f502
Norman-Ian Böttcher <norman.boettcher@web.de>
2026-02-21 21:28:56 +0100

    [#48] Big Refactoring - added a bom for crypto microservice

---
+++ bot-pilot-crypto/bot-pilot-crypto-infrastructure/infrastructure-adapter/vault-mapper/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $de.bot.pilot.crypto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
