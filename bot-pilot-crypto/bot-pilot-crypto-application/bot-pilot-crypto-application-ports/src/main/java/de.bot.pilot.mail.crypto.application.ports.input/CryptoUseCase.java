10145639d16456c73a83c659193d3cd79f6ebaf9
Norman-Ian Böttcher <norman.boettcher@web.de>
2026-02-21 14:50:15 +0100

    [#48] Added  - spring boot parent to bot-pilot-crypto

--- bot-pilot-crypto/bot-pilot-crypto-application/bot-pilot-crypto-application-ports/src/main/java/de.bot.pilot.mail.crypto.application.ports.input/CryptoUseCase.java
+++ bot-pilot-crypto/bot-pilot-crypto-application/bot-pilot-crypto-application-ports/src/main/java/de.bot.pilot.mail.crypto.application.ports.input/CryptoUseCase.java
@@ -1,4 +1,35 @@
 package de.bot.pilot.mail.crypto.application.ports.input;
 
+import de.bot.pilot.mail.crypto.domain.command.DecryptCommand;
+import de.bot.pilot.mail.crypto.domain.command.EncryptCommand;
+
+/**
+ * Provides cryptographic operations for encryption and decryption.
+ * This interface defines the contract for handling cryptographic processes
+ * by taking specialized command objects as input for both encryption
+ * and decryption routines.
+ */
 public interface CryptoUseCase {
+
+    /**
+     * Encrypts the given plaintext encapsulated in an {@code EncryptCommand}.
+     * The method processes the supplied command to generate an encrypted representation
+     * of the raw text value contained within the command.
+     *
+     * @param command the {@code EncryptCommand} containing the plaintext to be encrypted
+     *                and a unique identifier for the encryption request
+     * @return a {@code String} representing the encrypted version of the plaintext
+     */
+    String encrypt(EncryptCommand command);
+
+    /**
+     * Decrypts the given ciphertext encapsulated in a {@code DecryptCommand}.
+     * The method processes the supplied command to generate the plaintext
+     * representation of the encrypted data contained within the command.
+     *
+     * @param command the {@code DecryptCommand} containing the ciphertext to be decrypted
+     *                and a unique identifier for the decryption request
+     * @return a {@code String} representing the decrypted plaintext
+     */
+    String decrypt(DecryptCommand command);
 }
--- bot-pilot-crypto/bot-pilot-crypto-application/bot-pilot-crypto-application-service/src/main/java/de/bot/pilot/mail/crypto/application/service/CryptoService.java
+++ bot-pilot-crypto/bot-pilot-crypto-application/bot-pilot-crypto-application-service/src/main/java/de/bot/pilot/mail/crypto/application/service/CryptoService.java
@@ -1,4 +1,32 @@
 package de.bot.pilot.mail.crypto.application.service;
 
-public class CryptoService {
+import de.bot.pilot.mail.crypto.application.ports.input.CryptoUseCase;
+import de.bot.pilot.mail.crypto.domain.command.DecryptCommand;
+import de.bot.pilot.mail.crypto.domain.command.EncryptCommand;
+import de.bot.pilot.mail.crypto.domain.values.Ciphertext;
+import de.bot.pilot.mail.crypto.domain.values.Plaintext;
+import de.bot.pilot.mail.crypto.domain.values.ports.output.CryptoPort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CryptoService implements CryptoUseCase {
+
+    private final CryptoPort cryptoPort;
+
+    public CryptoService(CryptoPort cryptoPort) {
+        this.cryptoPort = cryptoPort;
+    }
+
+    @Override
+    public String encrypt(EncryptCommand command) {
+        var plaintext = new Plaintext(command.rawValue());
+        return cryptoPort.encrypt(plaintext).value();
+    }
+
+    @Override
+    public String decrypt(DecryptCommand command) {
+        var ciphertext = new Ciphertext(command.rawCiphertext());
+        return cryptoPort.decrypt(ciphertext).value();
+    }
+
 }
